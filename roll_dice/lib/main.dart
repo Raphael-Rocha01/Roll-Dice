cd029287ef06f963108dc962f6a273cf12d3bc59
Raphael Rocha <raphael-rocha-@hotmail.com>
2024-06-21 14:50:44 -0300

    aula 36

--- roll_dice/lib/main.dart
+++ roll_dice/lib/main.dart
@@ -4,10 +4,36 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello World"),
-        ),
+        body: Teste(),
       ),
     ),
   );
+}
+
+class Teste extends StatelessWidget {
+  const Teste({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
